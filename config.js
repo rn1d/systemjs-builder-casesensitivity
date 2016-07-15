b9bb412826b0f708aa315146a38ce4cfdb96f8bb
rn1d <x29a@me.com>
2016-07-15 21:56:39 +0300

    Added gitignored config.js file.

---
+++ config.js
@@ -0,0 +1,10 @@
+System.config({
+    baseURL: "/",
+    defaultJSExtensions: true,
+    paths: {
+        "npm:*": "node_modules/*"
+    },
+    map: {
+        "moment": "npm:moment/moment",
+    }
+});
